15439fd0ba178305e8d14f31ff12f8b1f27cb305
Md. Affan Hossain Rakib <affanhossainrakib@gmail.com>
2025-10-05 03:13:15 +0600

    MIGRATION to React Vite with JS, ShadcnUI

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center bg-foreground">
+      <Button variant="outline" size="sm">
+        New Branch
+      </Button>
+    </div>
+  )
+}
+
+export default App
