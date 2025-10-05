cbd94e34cf2add9dc99f387b258903472f05aa69
Md. Affan Hossain Rakib <affanhossainrakib@gmail.com>
2025-10-05 22:23:09 +0600

    feat: footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import { Github } from 'lucide-react'
+import { Link } from 'react-router'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className='flex justify-center items-center border-t'>
+            <Link to="https://github.com/AffanHossainRakib/" target='_blank'>
+                <Button className=" bg-[#cde3fd90] text-[#005ac4] hover:bg-[#cde3fd] dark:bg-[#00173190] dark:text-[#67aaf8] dark:hover:bg-[#001731]">
+                    <Github /> Md. Affan Hossain Rakib
+                </Button>
+            </Link>
+
+
+        </div>
     )
 }
 
